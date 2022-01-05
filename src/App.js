637cf08c6271e051c81ab74a2fbd6904cb278d91
Renato Tripi <renatosft@gmail.com>
2022-01-05 18:39:57 -0300

    banner section finished

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Home from './components/pages/Home';
+
 function App() {
   return (
     <>
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" exact />
+          <Route path="/" exact element={<Home />} />
         </Routes>
       </Router>
     </>
